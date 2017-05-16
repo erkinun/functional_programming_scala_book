d67a344fffb12a74ca5a8ede7629fdb856228ba2
lostMohican <erkinun@gmail.com>
2017-05-16 10:00:39 +0100

    isSorted and currying

--- src/main/scala/com/asyaminor/functional/book/Application.scala
+++ src/main/scala/com/asyaminor/functional/book/Application.scala
@@ -6,7 +6,9 @@ object Application  {
   def main(args: Array[String]): Unit = {
     println("hello world")
 
-    println(fib(6))
+    println(isSorted[Int](Array(1,2,3,4), ordered = (a: Int, b: Int) => a < b))
+    println(isSorted[Int](Array(1,3,2,4), ordered = (a: Int, b: Int) => a < b))
+    println(isSorted[Int](Array(4,3,2,1), ordered = (a: Int, b: Int) => a > b))
   }
 
   def factorial(n: Int): Int = {
@@ -27,5 +29,23 @@ object Application  {
     val (i1, i2) = go(n - 3, 0, 1)
 
     i1 + i2
+  }
+
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+
+    @tailrec
+    def loop(n: Int): Boolean = {
+      if (n >= as.length - 1) true
+      else if (ordered(as(n), as(n + 1))) loop(n + 1)
+      else false
+    }
+
+    loop(0)
+  }
+
+  def partial1[A,B,C](a: A, f: (A,B) => C): B => C = (b: B) => f(a, b)
+
+  def curry[A,B,C](f: (A, B) => C): A => (B => C) = {
+    (a: A) => partial1(a, f)
   }
 }
