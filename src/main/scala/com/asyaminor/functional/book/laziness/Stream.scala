f88efdf17c623c5854dd3d0e96fb55080d9edc6a
lostMohican <erkinun@gmail.com>
2017-06-23 10:16:28 +0100

    stream functions

---
+++ src/main/scala/com/asyaminor/functional/book/laziness/Stream.scala
@@ -0,0 +1,34 @@
+package com.asyaminor.functional.book.laziness
+
+sealed trait Stream[+A] {
+  def headOption: Option[A] = this match {
+    case Empty => None
+    case Cons(h, _) => Some(h())
+  }
+
+  def toList: List[A] = this match {
+    case Empty => Nil
+    case Cons(h, t) => h() :: t().toList
+  }
+
+  def take(n: Int): Stream[A] = this match {
+    case Empty => Empty
+    case Cons(h, t) => if (n == 0) Empty
+    else Cons(h, () => t().take(n-1))
+  }
+}
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+
+object Stream {
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  }
+
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A](as: A*): Stream[A] = if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+}
