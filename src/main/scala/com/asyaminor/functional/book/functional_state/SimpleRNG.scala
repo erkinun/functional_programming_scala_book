c8608c87af4e2579a365534c7c186a95bc59d8c8
lostMohican <erkinun@gmail.com>
2017-07-14 10:27:06 +0100

    map functions on rand

--- src/main/scala/com/asyaminor/functional/book/functional_state/SimpleRNG.scala
+++ src/main/scala/com/asyaminor/functional/book/functional_state/SimpleRNG.scala
@@ -2,6 +2,7 @@ package com.asyaminor.functional.book.functional_state
 
 trait RNG {
   def nextInt: (Int, RNG)
+
 }
 
 case class SimpleRNG(seed: Long) extends RNG {
@@ -14,6 +15,28 @@ case class SimpleRNG(seed: Long) extends RNG {
 }
 
 object SimpleRNG {
+
+  type Rand[+A] = RNG => (A, RNG)
+
+  def unit[A](a: A): Rand[A] = rng => (a, rng)
+  def int: Rand[Int] = rng => rng.nextInt
+  def map[A,B](s: Rand[A])(f: A => B): Rand[B] = rng => {
+    val (a, rng2) = s(rng)
+    (f(a), rng2)
+  }
+
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = rng => {
+    val (a, rng2) = ra(rng)
+    val (b, rng3) = rb(rng2)
+
+    (f(a, b), rng3)
+  }
+
+  def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] = map2(ra, rb)((_, _))
+
+  val randIntDouble: Rand[(Int, Double)] = both(int, double)
+  val randDoubleInt: Rand[(Double, Int)] = both(double, int)
+
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
     val (i1, rngNext) = rng.nextInt
 
@@ -21,6 +44,10 @@ object SimpleRNG {
     else if (i1 < 0) (Math.abs(i1), rngNext)
     else (i1, rngNext)
   }
+
+  def nonNegativeEven: Rand[Int] = map(nonNegativeInt)(i => i - i % 2)
+
+  def doubleM: Rand[Double] = map(nonNegativeInt)(i => i.toDouble / Int.MaxValue)
 
   def double(rng: RNG): (Double, RNG) = {
     val (nonNeg, next) = nonNegativeInt(rng)
