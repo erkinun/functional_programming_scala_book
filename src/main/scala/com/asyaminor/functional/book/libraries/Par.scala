f24aa13ab6a6da12c8998a38de3fc0c4f1a711f0
lostMohican <erkinun@gmail.com>
2017-07-28 10:58:29 +0100

    par library from book

---
+++ src/main/scala/com/asyaminor/functional/book/libraries/Par.scala
@@ -0,0 +1,34 @@
+package com.asyaminor.functional.book.libraries
+
+import java.util.concurrent.{Callable, ExecutorService, Future}
+
+import scala.concurrent.duration.TimeUnit
+
+
+object Par {
+  type Par[A] = ExecutorService => Future[A]
+
+  def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
+
+  def fork[A](a: => Par[A]): Par[A] =
+    es => es.submit(new Callable[A] {
+      override def call(): A = a(es).get()
+    })
+  //def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+  def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
+
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    def isDone = true
+    def get(timeout: Long, units: TimeUnit): A = get
+    def isCancelled = false
+    def cancel(evenIfRunning: Boolean): Boolean = false
+  }
+
+  def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] =
+    (es: ExecutorService) => {
+      val af = a(es)
+      val bf = b(es)
+
+      UnitFuture(f(af.get, bf.get))
+    }
+}
