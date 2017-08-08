c3e37ae15d72becaf2a747c984f3b4e548e96348
lostMohican <erkinun@gmail.com>
2017-08-08 10:35:05 +0100

    choice implemented for par

--- src/main/scala/com/asyaminor/functional/book/datastructures/Option.scala
+++ src/main/scala/com/asyaminor/functional/book/datastructures/Option.scala
@@ -50,6 +50,7 @@ object Option {
   def map3[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
     a.flatMap(av => b.map(bv => f(av,bv)))
 
+  // def flatMap[B](f: A => Option[B]): Option[B]
   def sequence[A](a: List[Option[A]]): Option[List[A]] =
     a match {
       case Nil => Some(Nil)
--- src/main/scala/com/asyaminor/functional/book/libraries/Par.scala
+++ src/main/scala/com/asyaminor/functional/book/libraries/Par.scala
@@ -10,6 +10,7 @@ object Par {
 
   def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
 
+  def delay[A](fa: => Par[A]): Par[A] = es => fa(es)
   def fork[A](a: => Par[A]): Par[A] =
     es => es.submit(new Callable[A] {
       override def call(): A = a(es).get()
@@ -32,5 +33,42 @@ object Par {
       UnitFuture(f(af.get, bf.get))
     }
 
+  def map[A,B](pa: Par[A])(f: A => B): Par[B] = map2(pa, unit(()))((a,_) => f(a))
+  def sortPar(parList: Par[List[Int]]): Par[List[Int]] = map(parList)(_.sorted)
   def asyncF[A,B](f: A => B): A => Par[B] = a => lazyUnit(f(a))
+
+  def parFilter[A](as: List[A])(f: A => Boolean): Par[List[A]] = fork {
+    val filtered = as.map(asyncF(a => if(f(a)) List(a) else List()))
+    map(sequence(filtered))(_.flatten)
+  }
+  def parMap[A,B](ps: List[A])(f: A => B): Par[List[B]] = fork {
+    val fbs: List[Par[B]] = ps.map(asyncF(f))
+    sequence(fbs)
+  }
+
+  // def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
+  def sequence[A](ps: List[Par[A]]): Par[List[A]] =
+    map(sequenceBalanced(ps.toIndexedSeq))(_.toList)
+
+  def sequenceBalanced[A](as: IndexedSeq[Par[A]]): Par[IndexedSeq[A]] = fork {
+    if (as.isEmpty) unit(Vector())
+    else if (as.length == 1) map(as.head)(a => Vector(a))
+    else {
+      val (l,r) = as.splitAt(as.length/2)
+      map2(sequenceBalanced(l), sequenceBalanced(r))(_ ++ _)
+    }
+  }
+
+  def choiceN[A](n: Par[Int])(choices: List[Par[A]]): Par[A] =
+    es => {
+      val nResult = run(es)(n).get()
+      choices(nResult)(es)
+    }
+
+  def choice[A](cond: Par[Boolean])(t: Par[A], f: Par[A]): Par[A] = {
+    val choiceSelector = map(cond)(b => if(b) 1 else 0)
+    val choices = List(f, t)
+
+    choiceN(choiceSelector)(choices)
+  }
 }
