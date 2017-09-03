76faeefe5aa169348a98246ef91669ea134fc705
lostMohican <erkinun@gmail.com>
2017-09-03 18:03:03 +0100

    organizing parser api and json parser work start

--- src/main/scala/com/asyaminor/functional/book/libraries/parser/Parsing.scala
+++ src/main/scala/com/asyaminor/functional/book/libraries/parser/Parsing.scala
@@ -1,5 +1,7 @@
 package com.asyaminor.functional.book.libraries.parser
 
+import scala.util.matching.Regex
+
 object Parsing {
 
 }
@@ -8,21 +10,27 @@ trait Parsers[ParseError, Parser[+_]] {
 
   self =>
 
+  // primitives
   implicit def string(s: String): Parser[String]
+  implicit def regex(r: Regex): Parser[String]
+  def slice[A](p: Parser[A]): Parser[String]
+  def flatMap[A,B](p: Parser[A])(f: A => Parser[B]): Parser[B]
+  def succeed[A](a: A): Parser[A] = string("") map (_ => a)
+  def or[A](s1: Parser[A], s2: => Parser[A]): Parser[A]
   implicit def operators[A](p: Parser[A]) = ParserOps[A](p)
   implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
 
   def run[A](p: Parser[A])(input: String): Either[ParseError,A]
   def char(c: Char): Parser[Char] = string(c.toString).map(_.charAt(0))
-  def or[A](s1: Parser[A], s2: => Parser[A]): Parser[A]
+
   //def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
   def many[A](p: Parser[A]): Parser[List[A]] = map2(p, many(p))(_ :: _) or succeed(List())
   def map[A,B](a: Parser[A])(f: A => B): Parser[B] = a.flatMap(aVal => succeed(f(aVal)))
 
-  def slice[A](p: Parser[A]): Parser[String]
+
   def product[A,B](p: Parser[A], p2: => Parser[B]): Parser[(A,B)] = p.flatMap(a => p2.flatMap(b => succeed((a, b))))
 
-  def succeed[A](a: A): Parser[A] = string("") map (_ => a)
+
 
 
   def map2[A,B,C](p: Parser[A], p2: => Parser[B])(f: (A,B) => C): Parser[C] = p.flatMap(a => p2.map(b => f(a,b)))
@@ -30,7 +38,7 @@ trait Parsers[ParseError, Parser[+_]] {
 
   def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]] = map2(succeed(n), p){ case (a, b) => Range(0, a).map(_ => b).toList }
 
-  def flatMap[A,B](p: Parser[A])(f: A => Parser[B]): Parser[B]
+
 
 
   case class ParserOps[A](p: Parser[A]) {
