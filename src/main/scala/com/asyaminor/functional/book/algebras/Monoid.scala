2d2e797f74b2da19acbf7129ff6210e5cd366c54
lostMohican <erkinun@gmail.com>
2017-10-03 23:13:56 +0100

    function and bag monoid

--- src/main/scala/com/asyaminor/functional/book/algebras/Monoid.scala
+++ src/main/scala/com/asyaminor/functional/book/algebras/Monoid.scala
@@ -153,4 +153,23 @@ object Monoid {
       override def zero: (A, B) = (A.zero, B.zero)
     }
   }
+
+  def mapMergeMonoid[K,V](V: Monoid[V]): Monoid[Map[K, V]] = new Monoid[Map[K, V]] {
+    def zero = Map[K,V]()
+    def op(a: Map[K, V], b: Map[K, V]) =
+      (a.keySet ++ b.keySet).foldLeft(zero) { (acc,k) =>
+        acc.updated(k, V.op(a.getOrElse(k, V.zero),
+          b.getOrElse(k, V.zero)))
+      } }
+
+  def functionMonoid[A,B](B: Monoid[B]): Monoid[A => B] = {
+    new Monoid[(A) => B] {
+      override def op(a1: (A) => B, a2: (A) => B): (A) => B = a => B.op(a1(a), a2(a))
+      override def zero: (A) => B = _ => B.zero
+    }
+  }
+
+  def bag[A](as: IndexedSeq[A]): Map[A, Int] = {
+    foldableSeq.foldMap(as)(a => Map(a -> 1))(mapMergeMonoid[A, Int](intAddition))
+  }
 }
