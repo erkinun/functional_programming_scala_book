52f7a2873e9ae4611a4b3a7c390bdcf10efe9eb7
lostMohican <erkinun@gmail.com>
2017-10-04 11:31:11 +0100

    monads are added

---
+++ src/main/scala/com/asyaminor/functional/book/algebras/Monad.scala
@@ -0,0 +1,38 @@
+package com.asyaminor.functional.book.algebras
+
+import com.asyaminor.functional.book.libraries.testing.Gen
+
+trait Functor[F[_]] {
+  def map[A,B](fa: F[A])(f: A => B): F[B]
+
+  def distribute[A,B](fab: F[(A, B)]): (F[A], F[B]) = (map(fab)(_._1), map(fab)(_._2))
+
+  def codistribute[A,B](e: Either[F[A], F[B]]): F[Either[A, B]] = e match {
+    case Left(fa) => map(fa)(Left(_))
+    case Right(fb) => map(fb)(Right(_))
+  }
+}
+
+trait Monad[F[_]] {
+
+  def unit[A](a: => A): F[A]
+  def flatMap[A,B](ma: F[A])(f: A => F[B]): F[B]
+
+  def map[A,B](ma: F[A])(f: A => B): F[B] = flatMap(ma)(a => unit(f(a)))
+
+  def map2[A,B,C](fa: F[A], fb: F[B])(f: (A,B) => C): F[C] =
+    flatMap(fa)(a => map(fb)(b => f(a,b)))
+}
+
+object Monad {
+
+  val listFunctor = new Functor[List] {
+    def map[A,B](as: List[A])(f: A => B): List[B] = as map f
+  }
+
+  val genMonad = new Monad[Gen] {
+    def unit[A](a: => A): Gen[A] = Gen.unit(a)
+    def flatMap[A,B](ma: Gen[A])(f: A => Gen[B]): Gen[B] =
+      ma flatMap f
+  }
+}
