04f84509fc45f29f260ec5fad6ec0310e755b966
lostMohican <erkinun@gmail.com>
2017-10-05 11:01:28 +0100

    sequence and traverse monad methods

--- src/main/scala/com/asyaminor/functional/book/algebras/Monad.scala
+++ src/main/scala/com/asyaminor/functional/book/algebras/Monad.scala
@@ -1,5 +1,9 @@
 package com.asyaminor.functional.book.algebras
 
+import java.time.MonthDay
+
+import com.asyaminor.functional.book.datastructures
+import com.asyaminor.functional.book.functional_state.State
 import com.asyaminor.functional.book.libraries.testing.Gen
 
 trait Functor[F[_]] {
@@ -22,6 +26,17 @@ trait Monad[F[_]] {
 
   def map2[A,B,C](fa: F[A], fb: F[B])(f: (A,B) => C): F[C] =
     flatMap(fa)(a => map(fb)(b => f(a,b)))
+
+
+  def sequence[A](lma: List[F[A]]): F[List[A]] = lma match {
+    case Nil => unit(Nil)
+    case h :: t => flatMap(h)(a => map(sequence(t))(listA => a :: listA))
+  }
+
+  def traverse[A,B](la: List[A])(f: A => F[B]): F[List[B]] = la match {
+    case Nil => unit(Nil)
+    case h :: t => flatMap(f(h))(b => map(traverse(t)(f))(list => b :: list))
+  }
 }
 
 object Monad {
@@ -35,4 +50,25 @@ object Monad {
     def flatMap[A,B](ma: Gen[A])(f: A => Gen[B]): Gen[B] =
       ma flatMap f
   }
+
+  val listMonad = new Monad[List] {
+    override def unit[A](a: => A) = List(a)
+    override def flatMap[A, B](ma: List[A])(f: (A) => List[B]) = ma.flatMap(a => f(a))
+  }
+
+  val streamMonad = new Monad[Stream] {
+    override def unit[A](a: => A) = Stream(a)
+    override def flatMap[A, B](ma: Stream[A])(f: (A) => Stream[B]) = ma.flatMap(a => f(a))
+  }
+
+  val optionMonad = new Monad[Option] {
+    override def unit[A](a: => A) = Option(a)
+    override def flatMap[A, B](ma: Option[A])(f: (A) => Option[B]) = ma.flatMap(a => f(a))
+  }
+
+//  val stateMonad = new Monad[State] {
+//    override def unit[A](a: => A) = State.unit(a)
+//    override def flatMap[A, B](ma: State[A])(f: (A) => State[B]) = ???
+//  }
+
 }
