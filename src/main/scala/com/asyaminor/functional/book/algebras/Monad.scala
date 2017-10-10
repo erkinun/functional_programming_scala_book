5e25588f85144ec5549dfc6e541cc2ac3e8d1dd1
lostMohican <erkinun@gmail.com>
2017-10-10 11:01:45 +0100

    flatmap in terms of compose

--- src/main/scala/com/asyaminor/functional/book/algebras/Monad.scala
+++ src/main/scala/com/asyaminor/functional/book/algebras/Monad.scala
@@ -20,7 +20,11 @@ trait Functor[F[_]] {
 trait Monad[F[_]] {
 
   def unit[A](a: => A): F[A]
-  def flatMap[A,B](ma: F[A])(f: A => F[B]): F[B]
+  def flatMap[A,B](ma: F[A])(f: A => F[B]): F[B] = {
+    val firstFunction: Unit => F[A] = _ => ma
+    val result: (Unit) => F[B] = compose(firstFunction, f)
+    result()
+  }
 
   def map[A,B](ma: F[A])(f: A => B): F[B] = flatMap(ma)(a => unit(f(a)))
 
@@ -60,7 +64,7 @@ object Monad {
 
   val genMonad = new Monad[Gen] {
     def unit[A](a: => A): Gen[A] = Gen.unit(a)
-    def flatMap[A,B](ma: Gen[A])(f: A => Gen[B]): Gen[B] =
+    override def flatMap[A,B](ma: Gen[A])(f: A => Gen[B]): Gen[B] =
       ma flatMap f
   }
 
